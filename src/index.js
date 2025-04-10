9adea3f104dd34be8fb6c0bc5a0843e715866519
Hiba AMEDJAR <117330946+Hiamedja@users.noreply.github.com>
2025-04-10 16:17:39 +0100

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import react from 'react';
-import ReactDOM from 'react-dom/client'; // Import from 'react-dom/client' instead of 'react-dom'
+import ReactDOM from 'react-dom/client';
 import App from './App';
 
-const root = ReactDOM.createRoot(document.getElementById('root')); // Create the root
-root.render(<App />); // Render the app
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(<App />);
