2e35187c9b3ab21665b7f538f456d78450acfa5f
Hiba AMEDJAR <117330946+Hiamedja@users.noreply.github.com>
2025-04-10 16:16:08 +0100

    Add files via upload

---
+++ src/index.js
@@ -0,0 +1,6 @@
+import react from 'react';
+import ReactDOM from 'react-dom/client'; // Import from 'react-dom/client' instead of 'react-dom'
+import App from './App';
+
+const root = ReactDOM.createRoot(document.getElementById('root')); // Create the root
+root.render(<App />); // Render the app
